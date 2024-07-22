e8ef6ff5c4acd5929cc6db80db76c6988e75e0b2
Mehta Mihir Jayeshkumar <mehtamihir2304@gmail.com>
2024-07-22 22:36:33 +0530

    Add files via upload

---
+++ config/dbcon.php
@@ -0,0 +1,24 @@
+<?php
+
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "phpecom";
+
+    
+    $con = mysqli_connect($host, $username, $password, $database);
+
+    if(!$con)
+    {
+        die("Connection Falied: ". mysqli_connect_error());
+    }
+    else
+    {
+
+    }
+
+
+
+
+?>
